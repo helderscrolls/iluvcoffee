cc31c807debf83f1f62daea22c42dbaffefb04db
Helder Nascimento <helder.nascimento@ext.vallourec.com>
2021-07-15 23:28:49 +0200

    feat: add patch and delete requests

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
+  @Get()
   findAll() {
     return 'This action returns all coffees';
   }
@@ -10,5 +18,20 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #${id} coffee`;
+  }
+
+  @Post()
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
