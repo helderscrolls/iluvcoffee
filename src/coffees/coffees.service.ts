dd7f05d1e2d75fbe9968dd7eafbfa287faf232aa
Helder Nascimento <helder.nascimento@ext.vallourec.com>
2021-07-16 00:52:02 +0200

    feat: create a REST API basic app

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,55 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+
+    return coffee;
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+
+    return createCoffeeDto;
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffe = this.findOne(id);
+
+    if (existingCoffe) {
+      // update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
